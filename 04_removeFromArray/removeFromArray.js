4fa4e089e6d811a47437403feb182ed8a1a9ecca
Dorin <gimiri99@gmail.com>
2023-10-05 23:40:19 -0400

    made the script to pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...valuesToRemove) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+        // Check if the current item is NOT in the valuesToRemove array
+        if (!valuesToRemove.includes(item)) {
+            // If it's not in valuesToRemove, add it to our new array
+            newArray.push(item);
+        }
+    }
+    return newArray;
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
