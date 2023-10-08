7edbd6321491a361d131cbf1aaca7bb8ebf72f99
Dorin <gimiri99@gmail.com>
2023-10-07 20:40:03 -0400

    completed the functions to pass the test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if ( num1 < 0 || num2 < 0 ){
+        return 'ERROR'
+    }
+
+    let smallest = num1;
+    let largest = num2;
+    let sum = 0;
+
+    if (num1 > num2){
+        smallest = num2;
+        largest = num1;
+    }
+
+    while ( smallest <= largest){
+        sum = sum + smallest;
+        smallest += 1;
+    }
+
+    return sum;
+    
 
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let num = (5 / 9) * (temperature - 32);
+
+  return Number(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let num = ((9 / 5) * temperature) + 32;
+
+  return Number(num.toFixed(1));
 };
 
 // Do not edit below this line
